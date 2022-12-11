7c690e3ba0867d73915f090789a13627f8b118cb
Saltpep <artem.morozov29@gmail.com>
2022-12-11 21:32:55 +0300

    Add example to main function

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from abc import ABC
 
 from config import *
 
+from path_finder import CityGraph
+
 
 class Courier(ABC):
     pass
@@ -27,3 +29,21 @@ class FootCourier(Courier):
         self.payment_per_hour = foot_courier_config.get("payment_per_hour")
         self.payment_for_the_order = foot_courier_config.get("payment_for_the_order")
         self.speed = foot_courier_config.get("average_speed")
+
+
+def main():
+    walk_graph = CityGraph(city_name, 'walk', False, 'time')
+
+    origin, distance_to_origin = walk_graph.get_nearest_node((64.54163541632319, 40.53513805881834))
+    destination, distance_to_destination = walk_graph.get_nearest_node((64.53597673079206, 40.534853703364995))
+
+    path = walk_graph.shortest_path(origin, destination)
+
+    print(walk_graph.path_length(path))
+
+    route_map = walk_graph.plot_path_folium(path)
+    route_map.save("./cache/city_map.html")
+
+
+if __name__ == '__main__':
+    main()
